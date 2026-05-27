5eeead7b04d92e96db5a7fcf9e78e5946fb26091
Tamara Heeffer <tamara@spheya.com>
2026-05-28 01:05:25 +0200

    optimize for trivially copyable types

--- ecs/archetype.hpp
+++ ecs/archetype.hpp
@@ -5,17 +5,16 @@
 #include <type_traits>
 #include <utility>
 
-#include "macros.hpp"
-#include "signature.hpp"
 #include "meta.hpp"
+#include "signature.hpp"
 
 namespace ecs {
 
 	struct column {
 		void (*destroy)(const void* ptr);
 		void (*mass_destroy)(const void* ptr, size_t count);
-		void (*move)(void* RESTRICT dst, void* RESTRICT src);
 		void (*mass_move)(void* RESTRICT dst, void* RESTRICT src, size_t count);
+		void (*move)(void* RESTRICT dst, void* RESTRICT src);
 
 		size_t element_size;
 		component_id component_id;
@@ -23,6 +22,8 @@ namespace ecs {
 
 		template<typename T, size_t S>
 		void init();
+
+		[[nodiscard]] bool is_trivially_copyable_data() const noexcept;
 	};
 
 	class archetype {
@@ -66,20 +67,29 @@ namespace ecs {
 	template<typename T, size_t S>
 	inline void column::init() {
 		static_assert(!std::is_reference_v<T>, "Column element type cannot be a reference");
-		destroy = [](const void* t) { static_cast<const T*>(t)->~T(); };
-		mass_destroy = [](const void* t, size_t count) {
-			for(size_t i = 0; i < count; ++i) static_cast<const T*>(t)[i].~T();
-		};
 
-		move = [](void* RESTRICT dst, void* RESTRICT src) { new(dst) T(std::move(*static_cast<T*>(src))); };
-		mass_move = [](void* RESTRICT dst, void* RESTRICT src, size_t count) {
-			for(size_t i = 0; i < count; ++i) new(static_cast<T*>(dst) + i) T(std::move(static_cast<T*>(src)[i]));
-		};
+		if constexpr(std::is_trivially_copyable_v<T>) {
+			destroy = nullptr;
+		} else {
+			destroy = [](const void* t) { static_cast<const T*>(t)->~T(); };
+			mass_destroy = [](const void* t, size_t count) {
+				for(size_t i = 0; i < count; ++i) static_cast<const T*>(t)[i].~T();
+			};
+
+			move = [](void* RESTRICT dst, void* RESTRICT src) { new(dst) T(std::move(*static_cast<T*>(src))); };
+			mass_move = [](void* RESTRICT dst, void* RESTRICT src, size_t count) {
+				for(size_t i = 0; i < count; ++i) new(static_cast<T*>(dst) + i) T(std::move(static_cast<T*>(src)[i]));
+			};
+		}
 
 		element_size = sizeof(T);
 		component_id = type_id<T>();
 
 		data = static_cast<char*>(malloc(S * sizeof(T)));
+	}
+
+	inline bool column::is_trivially_copyable_data() const noexcept {
+		return destroy == nullptr;
 	}
 
 	inline archetype::archetype(archetype&& other) noexcept :
@@ -110,11 +120,11 @@ namespace ecs {
 
 	inline archetype::~archetype() {
 		for(int i = 0; i < signature.size(); ++i) {
-			columns[i].mass_destroy(columns[i].data, size);
+			if(!columns->is_trivially_copyable_data()) columns[i].mass_destroy(columns[i].data, size);
 			free(columns[i].data);
 		}
 		free(entities);
-		delete[] columns;
+		free(columns);
 	}
 
 	template<typename... T>
@@ -122,9 +132,9 @@ namespace ecs {
 		static_assert(is_unique_v<T...>, "Archetype signature must only contain unique types");
 		static_assert((std::is_same_v<T, std::remove_cvref_t<T>> && ...), "Archetype signature only allows non-reference, unqualified types");
 		static_assert(!(std::is_same_v<T, entity> || ...), "Archetype signature cannot contain entity type");
-		columns = new column[sizeof...(T)];
 
 		constexpr size_t initial_capacity = 8;
+		columns = static_cast<column*>(malloc(sizeof...(T) * sizeof(column)));
 		(columns[signature.index_of<T>()].template init<T, initial_capacity>(), ...);
 		capacity = initial_capacity;
 		entities = static_cast<entity*>(malloc(capacity * sizeof(entity)));
@@ -159,8 +169,12 @@ namespace ecs {
 		entities[row] = entities[size];
 		for(int i = 0; i < signature.size(); ++i) {
 			column& column = columns[i];
-			column.destroy(column.data + column.element_size * row);
-			column.move(column.data + column.element_size * row, column.data + column.element_size * size);
+			if(column.is_trivially_copyable_data()) {
+			    memcpy(column.data + column.element_size * row, column.data + column.element_size * size, column.element_size);
+			} else {
+				column.destroy(column.data + column.element_size * row);
+				column.move(column.data + column.element_size * row, column.data + column.element_size * size);
+			}
 		}
 		return entities[row];
 	}
@@ -191,18 +205,21 @@ namespace ecs {
 			} else {
 				return signature.contains<T>();
 			}
-		}()
-		 && ...);
+		}() && ...);
 	}
 
 	inline void archetype::reallocate(size_t newCapacity) {
 		entities = static_cast<entity*>(realloc(entities, newCapacity * sizeof(entity)));
 		for(int i = 0; i < signature.size(); ++i) {
 			column& column = columns[i];
-			char* newData = static_cast<char*>(malloc(newCapacity * column.element_size)); // todo: padding, error handling
-			column.mass_move(newData, column.data, size);
-			free(column.data);
-			column.data = newData;
+			if(column.is_trivially_copyable_data()) {
+				column.data = static_cast<char*>(realloc(column.data, newCapacity * column.element_size));
+			} else {
+				char* newData = static_cast<char*>(malloc(newCapacity * column.element_size)); // todo: padding, error handling
+				column.mass_move(newData, column.data, size);
+				free(column.data);
+				column.data = newData;
+			}
 		}
 		capacity = newCapacity;
 	}
--- ecs/meta.hpp
+++ ecs/meta.hpp
@@ -4,6 +4,16 @@
 #include <cstdint>
 #include <type_traits>
 
+#if defined(__clang__)
+	#define RESTRICT __restrict__
+#elif defined(__GNUC__) || defined(__GNUG__)
+	#define RESTRICT __restrict__
+#elif defined(_MSC_VER)
+	#define RESTRICT __restrict
+#else
+	#define RESTRICT
+#endif
+
 namespace ecs {
 
     using type_index = uint32_t;
